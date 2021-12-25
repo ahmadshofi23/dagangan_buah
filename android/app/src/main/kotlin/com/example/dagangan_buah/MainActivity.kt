b420f079af05074ad5139d3cddb85ae8a028f2e0
ahmadshofi23 <ahmadshofihasibuan12@gmail.com>
2021-12-25 23:47:30 +0700

    add color and style text

---
+++ android/app/src/main/kotlin/com/example/dagangan_buah/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.dagangan_buah
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/shared/theme.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/cupertino.dart';
+
+Color kgreenColor = Color(0xff53B175);
+Color kblue1Color = Color(0xff5383EC);
+Color kblue2Color = Color(0xff4A66AC);
+Color kwhiteColor = Color(0xffFFFFFF);
+Color kgreyColor = Color(0xffFCFCFC);
+Color ktextColor = Color(0xffFFF9FF);
+Color kgrey2Color = Color(0xff828282);
+Color kblackColor = Color(0xff030303);
